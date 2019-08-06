424d2677bde1c0ff40116fc9e46e1dad60406f99
joaodematejr <joaodematejr@gmail.com>
2019-08-06 13:29:15 -0300

    Implementação formulario

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,152 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Webcam from "react-webcam";
 
-function App() {
+function MadeWithLove() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Built with love by the "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Material-UI
+      </Link>
+      {" team."}
+    </Typography>
   );
 }
 
-export default App;
+function salvarFoto() {
+  const imageSrc = this.webcam.getScreenshot();
+}
+
+const useStyles = makeStyles(theme => ({
+  "@global": {
+    body: {
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Webcam
+          audio={false}
+          height={350}
+          //ref={this.setRef}
+          screenshotFormat="image/jpeg"
+          width={350}
+          videoConstraints={{ width: 1280, height: 720, facingMode: "user" }}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          onClick={salvarFoto}
+          className={classes.submit}
+        >
+          Tirar Foto
+        </Button>
+        <Typography component="h1" variant="h5">
+          Cadastro
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="primeiroNome"
+                name="primeiroNome"
+                variant="outlined"
+                required
+                fullWidth
+                id="primeiroNome"
+                label="Primeiro Nome"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="ultimoNome"
+                label="Ultimo Nome"
+                name="ultimoNome"
+                autoComplete="ultimoNome"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="E-mail"
+                name="email"
+                autoComplete="e-mail"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="senha"
+                label="Senha"
+                type="password"
+                id="senha"
+                autoComplete="current-senha"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Confirmar Cadastro
+          </Button>
+        </form>
+      </div>
+      <Box mt={5}>
+        <MadeWithLove />
+      </Box>
+    </Container>
+  );
+}
